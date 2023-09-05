25d574469bc8d88e62782982e3580fad7528de20
cameron <camreedpeace@gmail.com>
2023-09-05 12:19:18 -0700

    input component rewrite

--- app/components/modals/GroupChatModal.tsx
+++ app/components/modals/GroupChatModal.tsx
@@ -6,11 +6,11 @@ import { useRouter } from "next/navigation";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { User } from "@prisma/client";
 
-import Input from "../inputs/Input";
 import Select from "../inputs/Select";
 import Modal from "./Modal";
 import Button from "../Button";
 import { toast } from "react-hot-toast";
+import Input from "../inputs/Input";
 
 interface GroupChatModalProps {
   isOpen?: boolean;
